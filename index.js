ac0c3f7a94d6fbea87f6b585098d952655146f30
Margaret Elson <margaret.a.elson@gmail.com>
2021-01-25 08:23:31 -0500

    All of the input sections are ready

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 let newTeamProfile = []
 
-var Manager = require("./lib/employee")
+var Employee = require("./lib/employee.js")
+var Manager = require("./lib/manager.js")
+var Engineer = require("./lib/engineer.js")
+var Intern = require("./lib/intern.js")
 
 
 function managerPrompt(){
@@ -56,9 +59,9 @@ function managerPrompt(){
             <div class="card" style="width: 18rem;">
             <div class="card-header">Team Manager: ${response.name}</div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Manager ID: ${response.ID}</li>
-                    <li class="list-group-item">Manager Email: ${response.email}</li>
-                    <li class="list-group-item">Manager Number${response.number}</li>
+                    <li class="list-group-item">ID: ${response.ID}</li>
+                    <li class="list-group-item">Email: ${response.email}</li>
+                    <li class="list-group-item">Office Number:${response.number}</li>
                 </ul>
             </div>
 
@@ -98,15 +101,52 @@ function engineerPrompt(){
             message: 'What is the Github username of the engineer?',
         },
     ]).then(function(response){
-        var engineerName = response.engineerName;
-        var engineerID = response.engineerID;
-        var engineerEmail = response.engineerEmail;
-        var engineerGithub = response.engineerNumber
-        const newTeamProfile = new Engineer(engineerName, engineerID, engineerEmail, engineerGithub);
+        var name = response.name;
+        var ID = response.ID;
+        var email = response.email;
+        var engineerGithub = response.engineerGithub
+        const newTeamProfile = new Engineer(name, ID, email, engineerGithub);
         finalTeam.push(newTeamProfile);
         teamMemPrompt();
+
+        const data = `
+        <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <link rel="stylesheet" href="style.css">
+            <title>My Team</title>
+            </head>
+            <body>
+                <nav class="navbar navbar-dark bg-dark">
+                    <span class="navbar-brand mb-0 h1 w-100 text-center">My Team</span>
+                    <p id="myTeam" class="lead"></p>
+                </nav>
+    
+                <div class="card" style="width: 18rem;">
+                <div class="card-header">Engineer: ${response.name}</div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${response.ID}</li>
+                        <li class="list-group-item">Email: ${response.email}</li>
+                        <li class="list-group-item">Github link: https://github.com/${response.engineerGithub}</li>
+                    </ul>
+                </div>
+    
+        
+        </body>
+    </html>
+        `
+    
+        fs.writeFile('index.html', data, (error) => {
+    
+            error ? console.error(error) : console.log('success!')
+    });
     })
-}    
+
+
 
 function internPrompt(){
     inquirer.prompt([
@@ -131,13 +171,49 @@ function internPrompt(){
             message: 'What school does the intern go to?',
         },
     ]).then(function(response){
-        var internName = response.internName;
-        var internID = response.internID;
-        var internEmail = response.internEmail;
-        var internSchool = response.internSchool
-        const newTeamProfile = new Intern(internName, internID, internEmail, internSchool);
+        var name = response.name;
+        var ID = response.ID;
+        var email = response.email;
+        var internSchool = response.internSchool;
+        const newTeamProfile = new Intern(name, ID, email, internSchool);
         finalTeam.push(newTeamProfile);
         teamMemPrompt();
+
+        const data = `
+        <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <link rel="stylesheet" href="style.css">
+            <title>My Team</title>
+            </head>
+            <body>
+                <nav class="navbar navbar-dark bg-dark">
+                    <span class="navbar-brand mb-0 h1 w-100 text-center">My Team</span>
+                    <p id="myTeam" class="lead"></p>
+                </nav>
+    
+                <div class="card" style="width: 18rem;">
+                <div class="card-header">Intern: ${response.name}</div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${response.ID}</li>
+                        <li class="list-group-item">Email: ${response.email}</li>
+                        <li class="list-group-item">School: ${response.internSchool}</li>
+                    </ul>
+                </div>
+    
+        
+        </body>
+    </html>
+        `
+    
+        fs.writeFile('index.html', data, (error) => {
+    
+            error ? console.error(error) : console.log('success!')
+    });
     })
 } 
     
